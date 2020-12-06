f8d8c3d398e1ea96eb7a7b70c4df20cc9cc1cd99
kush2522 <70235232+kush2522@users.noreply.github.com>
2020-12-06 23:23:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,15 @@
 from flask import Flask
 
 #Create a Flask constructor. It takes name of the current module as the argument
-app = Flask(_name_)
+app = Flask(__name__)
 
 #Create a route decorator to tell the application, which URL should be called for the #described function and define the function
 
 @app.route('/')
 def tutorialspoint():
-        return 'Hello world. This is demo task done for KlearNow.'
+   return 'Hello World. Task is successfully implemented over EKS'
 
-#Create the main driver function
-if _name_ == '_main_':
-#call the run method
-        app.run(host='0.0.0.0')
+    #Create the main driver function
+if __name__ == '__main__' :
+     #call the run method
+  app.run(host="0.0.0.0", debug=True)
